12adaf1fab56b58c4906865866023150397cc612
Ravanramsingh <ram19793@gmail.com>
2025-11-16 13:11:46 +0530

    Add AWS Security Group for web server

---
+++ securitygroup.tf
@@ -0,0 +1,36 @@
+# Create a Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  # Inbound Rules for SSH
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound Rules for HTTP
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound Rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "TerraformSG"
+  }
+}
